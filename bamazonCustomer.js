16542dde477564a6ba18e49c35cf0955ad9384d9
Aarick-F <zeflyndesign@gmail.com>
2018-05-01 18:28:57 -0400

    added a command-line table package to make formatting look better.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Table = require("cli-table");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+  host: process.env.HOST,
+  port: process.env.PORT,
   user: process.env.USER,
   password: process.env.MY_PASSWORD,
   database: process.env.MY_DATABASE
@@ -17,7 +18,7 @@ function homeMenu() {
     {
       name: "selection",
       type: "input",
-      message: "Product ID: "
+      message: "Please enter the ID of the product you would like to purchase..."
     }, {
       name: "quantity",
       type: "input",
@@ -25,13 +26,22 @@ function homeMenu() {
     }
   ])
   .then(data => {
-    console.log("Yep.");
+    if(parseInt(data.selection) === NaN) {
+      console.log("It looks like the item you selected wasn't an option," +
+                  "\nTaking you back to the home menu...");
+      homeMenu();
+    }
+    const query = "SELECT * FROM products";
+    connection.query(query, (err, res) => {
+      if(err) throw err;
+      console.log("GOT STUFF");
+    });
   });
 }
 
 function getProducts() {
   const query = "SELECT * FROM products";
-  connection.query(query, function(err, res) {
+  connection.query(query, (err, res) => {
     if(err) throw err;
     printProducts(res);
   });
@@ -41,12 +51,14 @@ function printProducts(products) {
   console.log("\n| Welcome to the Bamazonian Super General Store!" +
               "\n| Please take a look at our wares below!" +
               "\n| ==========================================");
-  console.log("\n| ID | ITEM | DEPARTMENT | PRICE");
-  products.forEach(product => {
-    console.log("| " + product.item_id + " | " + product.product_name + " | " +
-                product.department_name + " | $" + product.price);
+  let table = new Table({
+    head: ["ID", "ITEM", "DEPARTMENT", "PRICE"]
   });
-  console.log("\n| Please enter the ID of the product you wish to buy...");
+  products.forEach(product => {
+    table.push([product.item_id, product.product_name,
+                product.department_name, product.price]);
+  });
+  console.log(table.toString());
 }
 
 homeMenu();
